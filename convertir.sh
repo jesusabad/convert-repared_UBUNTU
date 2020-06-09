b52897004ca51602be250f24d181953752f050d2
Jesús Abad <jesusabad@users.noreply.github.com>
2020-06-09 11:28:36 +0000

    Actualización a la versión 1.1
    Pequeñas mejoras tras un primer día de uso.

--- convertir.sh
+++ convertir.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-# Versión 1.0
+# Versión 1.1
 # VARIABLES Y FORMATOS
-array_argumentos=($@)
-var_numero_argumentos=$#
+declare -a array_argumentos=($@)
+declare -i var_numero_argumentos=$#
 col_yellow='\033[1;33m'
+col_red='\033[31m'
 nc='\033[0m'
 
-printf "${col_yellow}Ejecutando script${nc}\n"
+printf "${col_yellow}Ejecutando script ...${nc}\n"
 
 # COMPROBACIÓN DE LA INSTALACIÓN DEL SCRIPT
 if [ ! -e "/usr/bin/convertir" ]; then
-        printf "${col_yellow}No existe el enlace simbólico, así que lo creo.${nc}\n"
+        printf "${col_red}No existe el enlace simbólico, así que lo creo.${nc}\n"
         sudo ln -s $PWD/convertir.sh /usr/bin/convertir
+fi
+
+# COMPROBACIÓN DE SI SE HAN INTRODUCIDO ARGUMENTOS
+if [ $var_numero_argumentos = 0 ]; then
+        printf "${col_red}No has introducido argumentos. Por ejemplo 'convertir *.jpg resultado.pdf'${nc}\n"
+        exit 1
 fi
 
 # MODIFICO EL ARCHIVO `policy.xml` A FIN DE EVITAR LA LIMITACIÓN DE `convert`
